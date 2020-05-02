85dba82098bbf487f8a7b4cbb26afa26a7344d2d
Alejandro Biedma <nabiedma@gmail.com>
2020-05-02 20:14:30 -0300

    working with lists

--- unit_3/lists.py
+++ unit_3/lists.py
@@ -1,5 +1,18 @@
 names = ['Roberto', 'Franku', 'Ale']
 print(names[2])
 
-message = f"Hey there ${names[1]}"
+message = f"Hey there {names[1]}"
 print(message)
+
+print("//////////////")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# append() adds an element to the end of the list
+motorcycles.append('ducati')
+print(motorcycles)
+
+# insert(position, value) adds a value at the specified index
+motorcycles.insert(0, 'kawasaki')
+print(motorcycles)
