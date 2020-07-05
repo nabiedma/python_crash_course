fe44daa924d50f4c2f04decfa52b0c156fd76744
nabiedma <nabiedma@gmail.com>
2020-07-04 23:53:34 -0300

    started unit 5

---
+++ unit_5/cars.py
@@ -0,0 +1,13 @@
+# IF
+# if CONDITION:
+#   PASS
+# else:
+#   PASS
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
